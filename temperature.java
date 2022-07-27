9ff6bad9ca0a51cfda5f103e3ad7b155f774962c
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 10:23:33 -0400

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class temperature {
+    public static void main (String args[])  
+    { float Fahrenheit, Celsius;  
+          Celsius= 35;  
+          Fahrenheit =((Celsius*9)/5)+32;  
+          System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);  
+    }
+}  
+
