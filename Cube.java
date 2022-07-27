cf60cadb25680dfef58514a4264fb4ca45157c3e
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 02:25:51 -0400

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Cube {
+     public static void main(String args[])  
+    {  
+    int a=5;  
+    int volume=a*a*a;  
+        System.out.println("Volume ot the cube="+volume);  
+     }  
+}  
+
