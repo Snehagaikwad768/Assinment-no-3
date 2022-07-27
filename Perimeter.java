851de4603f1164270cdc2afba7046616a9d600c0
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 02:32:11 -0400

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Perimeter {
+    public static void main (String args[])  
+    {      float a ,b, c, d, perimeter;  
+                    a=c= 5;  
+                    b=d=4;  
+                    perimeter  = 2*(a+b);  
+            System.out.println("Perimeter of Rectangle is: "+perimeter);  
+    }
+}  
+
