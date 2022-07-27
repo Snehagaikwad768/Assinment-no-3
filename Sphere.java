6ede594eb07c79926f12c15ac3e2a8822c1d9b82
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 02:14:57 -0400

    Add files via upload

---
+++ Sphere.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sphere {
+    public static void main(String args[])  
+    {  
+    int radius=6;  
+    double pie=3.14285714286;  
+    double volume=(4.0/3.0)*pie*(radius*radius*radius);  
+    System.out.println("Volume of the sphere="+volume);  
+     }  
+}  
+
