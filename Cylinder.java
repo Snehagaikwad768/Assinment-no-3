ac4f086e65fbbb0fef18620fffaa7ab400c4f1aa
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 02:28:27 -0400

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Cylinder {
+    public static void main(String args[])  
+    {  
+    int height=8;  
+    int radius=6;  
+    double pie=3.14285714286;  
+    double volume=pie*(radius*radius)*height;  
+        System.out.println("Volume of the cylinder="+volume);  
+     }  
+}  
+
