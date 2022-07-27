37e6d5c3aeffa49fee62a6eefb91b1ad2f2e6041
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-27 10:43:56 -0400

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Celsius {
+    public static void main (String args[])  
+    { float Fahrenheit, Celsius;  
+          Fahrenheit = 43;  
+          Celsius  = ((Fahrenheit-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+Celsius);  
+    }
+}  
+
