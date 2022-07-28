25be7ad08ea02101c95b369eac8786625d0060b4
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 01:56:20 -0400

    Add files via upload

--- Sphere.java
+++ Sphere.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package javaapplication1;
 
 /**
  *
@@ -12,9 +13,8 @@ public class Sphere {
     public static void main(String args[])  
     {  
     int radius=6;  
-    double pie=3.14285714286;  
+    double pie=3.14;  
     double volume=(4.0/3.0)*pie*(radius*radius*radius);  
     System.out.println("Volume of the sphere="+volume);  
      }  
-}  
-
+}
