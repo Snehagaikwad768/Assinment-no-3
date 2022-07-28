43ab182967f8185b7632dc9324934d48a4737e53
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 01:07:47 -0400

    Add files via upload

---
+++ Exercise9.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Exercise9 {
+    public static void main(String[] arg) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
+
