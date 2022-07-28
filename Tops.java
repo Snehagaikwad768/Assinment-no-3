f8d82ff2fbfa62066be271b0ee57dd93fb9b8a94
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 12:20:32 -0400

    Add files via upload

---
+++ Tops.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Tops {
+    public static void main (String args[])  
+    {   float a = 5f ;   
+                          double area = ( 1.73 * a*a) / 4 ;  
+System.out.println("Area of Equilateral Triangle is:"+area);  
+    }}  
+
