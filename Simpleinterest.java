d96c9330cb05659ddecbbcfbbe994d89a0d06e7f
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 12:24:40 -0400

    Add files via upload

---
+++ Simpleinterest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Simpleinterest {
+    public static void main (String args[])  
+    {   float p, r,  t,  si; // principal amount, rate, time and simple interest respectively  
+              p = 13000;  r = 12; t = 2;  
+               si  = (p*r*t)/100;   
+              System.out.println("Simple Interest is: " +si);  
+    }}  
+
