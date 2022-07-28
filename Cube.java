46d6767be18016e9ce5df40c63afac04611d468c
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 01:59:37 -0400

    Add files via upload

--- Cube.java
+++ Cube.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package javaapplication1;
 
 /**
  *
  * @author Lenovo
  */
 public class Cube {
-     public static void main(String args[])  
+    public static void main(String args[])  
     {  
     int a=5;  
     int volume=a*a*a;  
