602e7f5a9a425b020eb06fc6eff300864b7f7ab9
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 01:13:09 -0400

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TypeCasting {
+    public static void main(String[] args) {
+    // create int type variable
+    int num = 10;
+    System.out.println("The integer value: " + num);
+
+    // convert into double type
+    double data = num;
+    System.out.println("The double value: " + data);
+  }
+}
+
