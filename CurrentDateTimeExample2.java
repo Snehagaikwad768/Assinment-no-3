fbb19524338e8e7d184232bfc83a2043291946bb
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-28 01:02:15 -0400

    Add files via upload

---
+++ CurrentDateTimeExample2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.text.SimpleDateFormat;  
+import java.util.Date;
+public class CurrentDateTimeExample2 {
+    public static void main(String[] args) {  
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+    Date date = new Date();  
+    System.out.println(formatter.format(date));  
+}  
+}  
+
+
