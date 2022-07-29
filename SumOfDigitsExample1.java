7baefad008a5e8a9ee20c09a7739d1925c7d7a43
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-29 07:29:29 -0400

    Add files via upload

---
+++ SumOfDigitsExample1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;  
+public class SumOfDigitsExample1 {
+    {  
+public static void main (String args[])  
+{  
+int number, digit, sum = 0;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the number: ");  
+number = sc.nextInt();  
+while(number > 0)  
+{  
+//finds the last digit of the given number    
+digit = number % 10;  
+//adds last digit to the variable sum  
+sum = sum + digit;  
+//removes the last digit from the number  
+number = number / 10;  
+}  
+//prints the result  
+System.out.println("Sum of Digits: "+sum);  
+}  
+}  
+}
+
