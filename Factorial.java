a3579209b05518d6324771a3216bedc966d4bb89
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-29 08:11:51 -0400

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Factorial {
+     public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
+
